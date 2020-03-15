921761efff4f186fbfa908d429f2f83cce8b532d
Pietro Rampazzo <pietro@rampazzo.co>
2020-03-15 12:11:04 +0100

    Add custom bio + colors

--- src/gatsby-theme-blog/components/bio-content.js
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -1,16 +1,10 @@
 import React, { Fragment } from "react"
 import { Styled } from "theme-ui"
 
-/**
- * Change the content to add your own bio
- */
-
 export default () => (
   <Fragment>
-    This is where <Styled.a href="http://example.com/">your name</Styled.a>
-    {` `}
-    goes.
+    Hi, I'm <Styled.a href="/about/">Pietro</Styled.a>.
     <br />
-    Or whatever, you make the rules.
+    Computer Science Student
   </Fragment>
 )
