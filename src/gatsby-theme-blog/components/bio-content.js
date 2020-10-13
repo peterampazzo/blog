73633c0e366e6a4d31aa7a021a3bf122c02823fc
Pietro Rampazzo <pietro@rampazzo.co>
2020-10-13 09:59:56 +0200

    shorter bio

--- src/gatsby-theme-blog/components/bio-content.js
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -1,10 +1,9 @@
 import React, { Fragment } from "react"
-import { Styled } from "theme-ui"
 
 export default () => (
   <Fragment>
-    Hi, I'm <Styled.a href="/about/">Pietro</Styled.a>.
+    Hi, I'm Pietro.
     <br />
-    MSc Student in Human-Centered AI
+    Some guy <strike>in</strike> from Padova.
   </Fragment>
 )
